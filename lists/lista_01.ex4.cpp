e206bce9d3a28705ce60b4881eca9612e0cf247c
Maria Luciene <marialucienefelix@gmail.com>
2015-08-21 19:58:52 -0300

    Exercice 04 [Fineshed]

--- lists/lista_01.ex4.cpp
+++ lists/lista_01.ex4.cpp
@@ -3,59 +3,83 @@
 
 #include <iostream>
 #include <string>
-
 #define MAX 20
 #define INDEX 5
 
 using namespace std;
 
+typedef struct index
+{
+	int position;
+	int value;
+}Index;
+
+
 int search(int vector[], int element, int position){
+  int result = -1;
+
   cout << endl << "Position " << position << endl;
   for(int i = position; i<MAX ; i++){
-    if(vector[i]==element)
-      return i;
+    if(vector[i] == element){
+      cout << "entrou no for" << endl;
+      result =  i;
+    }
   }
-  return -1;
+  return result;
 }
 
-void preindex(int vector[], int index[]){
+int search_table(int vector[], Index index[], int number) {
+  int i = 0;
+  int result = 0;
+  while(number >= index[i].value && i<INDEX){
+  	i++;
+  }
+
+  cout << "I: " << i << endl;
+  result = search(vector,number,index[i-1].position);
+  return result;
+}
+void index_table(int vector[], Index index[]){
   int l = 0;
   for(int i = 0; i<INDEX ; i++) {
-    index[i] = l;
+    index[i].value = vector[l];            // Guarda a posição do numero pertencente ao vetor
+    index[i].position = l;         // Popula o posição dos valores no índice
     l+=4;
   }
 }
 
-void print(int vector[], int size){
-  for(int i=0; i<size ; i++){
+
+void print(int vector[]){
+  for(int i = 0; i < MAX ; i++){
     cout << vector[i] << " ";
   }
 }
 
-int search_table(int vector[], int index[], int number) {
-  int i = 0;
-  while(number>=index[i] && i<INDEX) i++;
-  return search(vector,number,index[i-1]);
+void print_index(Index index[]) {
+  for (int i = 0; i < INDEX; ++i) {
+    cout << index[i].value << " ";
+  }
 }
+
 
 int main(){
 
-  int index[INDEX];
+  Index index[INDEX];
   int vector[MAX] = {1,3,12,23,46,67,87,99,120,150,160,177,182,203,230,254,311,313,369,389};
   int element = 0;
 
-  preindex(vector,index);
+  index_table(vector,index);
   cout << "Vector " << endl;
-  print(vector,MAX);
+  print(vector);
   cout << endl << "Index " << endl;
-  print(index, INDEX);
+  print_index(index);
 
   cout << endl << "Insert the number" << endl;
   cin >> element;
 
   int result = search_table(vector,index,element);
 
-  if(result!=-1){
+  if(result == -1){
     cout << "Element not found" << endl;
   }else{
     cout << "Element found on position " << result << endl;
