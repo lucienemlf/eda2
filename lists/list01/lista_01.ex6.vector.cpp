e7c24d8d6749c91dc768c6f6eb661d7bc40d4315
Paulo Tada <paulohtfs@gmail.com>
2015-09-05 22:52:56 -0300

    Adding the exercise 6 and test_cases

--- lists/list01/lista_01.ex6.vector.cpp
+++ lists/list01/lista_01.ex6.vector.cpp
@@ -1,6 +1,7 @@
 // Binary Search
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
@@ -8,8 +9,9 @@ using namespace std;
 int binary_search(int vector[], int element, int min, int max){
   int result = -1;
   while(min<=max){
-    int mid = min + (max - min)*((element-vector[min])/(vector[max]-vector[min]));
-    //int mid = (max-min)/2 + min;
+    // Modification to use interpol on binary search
+    //int mid = min + (max - min)*((element-vector[min])/(vector[max]-vector[min]));
+    int mid = (max-min)/2 + min;
     if(vector[mid] == element){
       result = mid;
       break;
@@ -29,30 +31,25 @@ void preconfig(int vector[], int max_size){
   }
 }
 
-// Only for debug
-void print_vector(int vector[], int max_size){
-  for(int i = 0; i<max_size ; i++){
-    cout << vector[i] << " ";
-  }
-  cout << endl;
-}
-
 int main(){
-  int max_size = 0;
-  cout << "Enter the max size:";
-  cin >> max_size;
-  int vector[max_size];
-  preconfig(vector, max_size);
+  int max_size = 0; // Max size of the vector
+  while(1){
+    cin >> max_size;
+    if(max_size==-1) break;
 
-  int element = 0;
-  cout << "Number to find: ";
-  cin >> element;
+    int vector[max_size];
+    preconfig(vector, max_size); // Creates a vector with multiples of 2
+    int element = 0; // Element to find
+    cin >> element;
+    
+    clock_t start, end;
+    start = clock();
+    binary_search(vector,element,0,max_size-1);
+    end = clock();
+    
+    long double tmili = (end - start)/(double)(CLOCKS_PER_SEC/1000);
+    cout << "Time: " << tmili << " ms" << endl;
 
-  int searching = binary_search(vector,element,0,max_size-1);
-  if(searching == -1){
-    cout << "Element not found" << endl;
-  } else {
-    cout << "Element found on position " << searching << endl;
   }
   return 0;
 }
