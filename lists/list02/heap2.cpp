882607b41fb8e2c10bf4dc0d55b028b5213df05d
Paulo Tada <paulohtfs@gmail.com>
2015-09-21 22:03:52 -0300

    Adding heap

--- lists/list02/heap2.cpp
+++ lists/list02/heap2.cpp
@@ -1,38 +1,88 @@
 #include <iostream>
 #include <cmath>
-#include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 #include "vector_preconfig.h"
-#include "heap.h"
 
-using namespace std;
+int parent(int position);
+int left_child(int position);
+int right_child(int position);
 
-/* 
-   Coloca o maior elemento na ultima posição e 
-   realiza um heapify nos elementos restantes
-   fazendo com que o maior apareça na primeira 
-   posição novamente.
-*/
+void build_heap();
+void heapfy(int vector[], int size, int position);
+void heap_sort(int vector[]);
+void insert(int vector[], int element, int size);
+int extract_max(int vector[]);
+int get_max(int vector[], int dad);
 
-void heap_sort(int array[], int array_size){
-  int sort[array_size];
-  // Insert random number to the array size: array_size
-  for(int i = 1; i<=array_size ; i++){
-    insert_heap(array,random_num(),i);
-  }
+int main(){
+  int vector[10];
+  for(int i=0; i<10 ; i++)
+    insert(vector,i*3,10);
 
-  // Remove from a array to a order array
-  for(int i = array_size; i>=0 ; i--){
-    extract_max(array,sort,i);
+  print_vector(vector,10);
+  return 0;
+}
+
+void heapify(int vector[], int dad){
+  int larger = get_max(vector,dad);
+
+  if(larger != dad){
+    swap(vector[dad], vector[larger]);
+    heapify(vector,larger);
   }
 }
 
-int main(){
-  int max_size = 10;
-  int array[max_size];
-  preconfig(array,max_size);
-  print_vector(array,max_size);
-	heap_sort(array,max_size);	
-  print_vector(array,max_size);
-	return 0;
+int get_max(int vector[], int dad){
+  int left = left_child(dad);
+  int right = right_child(dad);
+  int larger = 0;
+  
+  if(left <= sizeof(vector) && vector[left] > vector[dad]){
+    larger = left;
+  }else{
+    larger = dad;
+  }
+
+  if(right <= sizeof(vector) && vector[right] > vector[dad]){
+    larger = right;
+  }
+
+  return larger;
+}
+
+int extract_max(int vector[]){
+  int size = sizeof(vector);
+  int max_int = vector[0];
+
+  swap(vector[0], vector[size-1]);
+  vector = (int*)realloc(vector,sizeof(vector)-4);
+  return max_int;
+}
+
+void insert(int vector[], int element, int size){
+  vector[size] = element;
+  
+  int position = (sizeof(vector)/4) - 1;
+  vector = (int*)realloc(vector,sizeof(vector)+4);
+
+  int dad = parent(position);
+  while(position>1 && vector[dad]<vector[position]){
+    swap(vector[dad],vector[position]);
+  }
+}
+
+int parent(int position){
+  int parent = floor(position/2);
+  return parent;
+}
+
+int left_child(int position){
+  int left_child = position*2;
+  return left_child;
+}
+
+int right_child(int position){
+  int right_child = position*2 + 1;
+  return right_child;
 }
